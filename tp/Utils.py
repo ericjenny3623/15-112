7860bca5e3d7665e8668074ca788958d22adbc40
eric <erc.hakawa@gmail.com>
2019-11-23 20:21:06 -0500

    added limit and set position

--- tp/Utils.py
+++ tp/Utils.py
@@ -13,12 +13,20 @@ def distance(x1, y1, x2, y2):
     return ((x1-x2)**2 + (y1-y2)**2)**0.5
 
 
+def limit(x, max, min):
+    assert max > min, "Max not greater than min"
+    if x > max:
+        return max
+    elif x < min:
+        return min
+    else:
+        return x
+
+
 class Twist():
 
     def __init__(self, x=0.0, y=0.0, heading=0.0, r=0.0):
-        self.x = 0.0
-        self.y = 0.0
-        self.heading, self.r = self.smartUpdateOrientation(heading, r)
+        self.setPosition(x, y, heading, r)
 
     def smartUpdateOrientation(self, heading, r):
         if heading != 0.0:
@@ -39,3 +47,10 @@ class Twist():
             deltaHeading, deltaR)
         self.heading += deltaHeading
         self.r += deltaR
+
+    def setPosition(self, x=None, y=None, heading=0.0, r=0.0):
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+        self.heading, self.r = self.smartUpdateOrientation(heading, r)
