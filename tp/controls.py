64010452b782e7ac20085681c5314d698bce4e74
eric <erc.hakawa@gmail.com>
2019-11-24 23:14:40 -0500

    path following working

--- tp/controls.py
+++ tp/controls.py
@@ -1,93 +1,134 @@
 import math
+import Utils
 
 
 class Controls():
 
-    def updatePursuit(robotPose):
-        curWaypoint = waypoints.get(index)
-        pose = robotPose
+    def __init__(self, waypoints, robot):
+        self.robot = robot
+        self.waypoints = waypoints
+        self.index = 0
+        self.isFinished = False
+
+        self.UPDATE_RATE = 100
+
+        self.kRadiusCritical = 0.1
+        self.kEpsilonCritical = 3.0
+        self.kRadiusPath = 0.0
+        self.kEpsilonPath = 5.0
+
+        self.MAX_SPEED = 3.3
+
+        self.kV = 1.0 / 14.0 * 14.0
+        self.kTurn = 0.5 / 360.0
+
+    def updatePursuit(self):
+        curWaypoint = self.waypoints[self.index]
+        robotPose = self.robot.center
 
         feedForwardSpeed = curWaypoint.kSpeed
+        distanceToWaypoint = Utils.distance(robotPose.x, robotPose.y,
+                                            curWaypoint.x, curWaypoint.y)
         debug = False
-        if curWaypoint.isCritical:  # important to be at exactly
+        if curWaypoint.isCritical or self.index == len(self.waypoints) - 1:  # important to be at exactly
 
-            if distanceFromWaypoint < math.abs(feedForwardSpeed) * 1.2:
+            if distanceToWaypoint < abs(feedForwardSpeed) * 1.2:
                 # speed reduces as distance gets smaller
-                feedForwardSpeed = math.copySign(
-                    distanceFromWaypoint / 1.2, feedForwardSpeed)
-                if math.abs(feedForwardSpeed) < 0.25:
-                    feedForwardSpeed = math.copySign(0.25, feedForwardSpeed)
+                feedForwardSpeed = math.copysign(
+                    distanceToWaypoint / 1.2, feedForwardSpeed)
+                if abs(feedForwardSpeed) < 0.25:
+                    feedForwardSpeed = math.copysign(0.25, feedForwardSpeed)
 
-            if atWaypoint(kRadiusCritical) or isFinished:
-                debug = true
+            if Utils.withinThreshold(distanceToWaypoint, 0.0, self.kRadiusCritical) or self.isFinished:
+                debug = True
                 feedForwardSpeed = 0.0
-                if atHeading(kEpsilonCritical):  # at point and heading, we're done
-                    if not isFinished:
+                # at point and heading, we're done
+                if Utils.withinThreshold(robotPose.heading, curWaypoint.heading, self.kEpsilonCritical):
+                    if not self.isFinished:
                         print(
-                            f"At Waypoint: {index} ({curWaypoint.toString()})")
-                    if index == len(waypoints) - 1 or isFinished:
-                        if not isFinished:
+                            f"At Waypoint: {self.index} ({curWaypoint.toString()})")
+                    if self.index == len(self.waypoints) - 1 or self.isFinished:
+                        if not self.isFinished:
                             print("Finished Path Following")
-                        isFinished = True
+                        self.isFinished = True
                         return 0.0, 0.0
                     else:
-                        index += 1
-                        curWaypoint = waypoints[index]
+                        self.index += 1
+                        curWaypoint = self.waypoints[self.index]
 
                 else:
                     # at point but not heading, just turn to the point
-                    ptrOutput = DrivetrainControls.turnToAngle(
-                        curWaypoint.heading, pose.heading)
-                    return DrivetrainControls.curvatureDrive(0.0, ptrOutput, True)
+                    # ptrOutput = DrivetrainControls.turnToAngle(
+                    #     curWaypoint.heading, robotPose.heading)
+                    # return curvatureDrive(0.0, ptrOutput, True)
+                    return 0.0, 0.0
 
-        elif atWaypoint(kRadiusPath) and atHeading(kEpsilonPath):
+        elif Utils.withinThreshold(distanceToWaypoint, 0.0, self.kRadiusPath) \
+                and Utils.withinThreshold(robotPose.heading, curWaypoint.heading, self.kEpsilonPath):
             # at non-critical waypoint
-            print(f"At Waypoint: {index} ({curWaypoint.toString()})")
-            index += 1
-            curWaypoint = waypoints[index]
+            print(f"At Waypoint: {self.index} ({curWaypoint.toString()})")
+            self.index += 1
+            curWaypoint = self.waypoints[self.index]
             debug = True
 
         # if not in a special case, just run path following
-        return pathFollowing()
+        return self.pathFollowing(robotPose, feedForwardSpeed)
 
-        def pathFollowing():
-            calculateDistanceFromWaypoint()
+    def pathFollowing(self, robotPose, ffSpeed):
+        curWaypoint = self.waypoints[self.index]
+        robotPose = self.robot.center
+        distanceToWaypoint = Utils.distance(robotPose.x, robotPose.y,
+                                            curWaypoint.x, curWaypoint.y)
+        print(curWaypoint)
 
-            straightPathAngle = math.atan2(curWaypoint.x - pose.x, curWaypoint.y - pose.y)
-            relativeAngle = pose.r - straightPathAngle
-            relativeOpposDist = distanceFromWaypoint * math.sin(relativeAngle)
-            relativeAdjacDist = distanceFromWaypoint * math.cos(relativeAngle)
-            relativeGoalAngle = pose.r - curWaypoint.r
-            # relativeGoalAngle = Utils.limit(relativeGoalAngle, math.PI/3.0,
-            # -math.PI/3.0)
-            relativeGoalDeriv = math.atan(relativeGoalAngle)
+        straightPathAngle = math.atan2(
+            curWaypoint.x - robotPose.x, curWaypoint.y - robotPose.y)
+        relativeAngle = robotPose.r - straightPathAngle
+        relativeOpposDist = distanceToWaypoint * math.sin(relativeAngle)
+        relativeAdjacDist = distanceToWaypoint * math.cos(relativeAngle)
+        relativeGoalAngle = robotPose.r - curWaypoint.r
+        # relativeGoalAngle = Utils.limit(relativeGoalAngle, math.PI/3.0,
+        # -math.PI/3.0)
+        relativeGoalDeriv = math.atan(relativeGoalAngle)
+        print("====", relativeAngle)
+        if relativeAdjacDist == 0:
+            print()
+        a, b = self.generateSpline(
+            relativeAdjacDist, relativeOpposDist, relativeGoalDeriv)
 
-            generateSpline(relativeAdjacDist, relativeOpposDist, relativeGoalDeriv)
+        nextSpeed = ((self.MAX_SPEED * ffSpeed) * 0.1) + \
+            (robotPose.velocity * 0.9)
+        deltaX = nextSpeed / self.UPDATE_RATE
+        if (deltaX < 0.0) == (ffSpeed > 0.0):
+            deltaX = 0.0
 
-            nextSpeed = ((MAX_SPEED * feedForwardSpeed) * 0.1) + (pose.velocity * 0.9)
-            deltaX = nextSpeed / UPDATE_RATE
-            if math.signum(deltaX) != math.signum(feedForwardSpeed):
-                deltaX = 0.0
+        if deltaX != 0.0:
+            y2 = (a * deltaX * deltaX * deltaX) + (b * deltaX * deltaX)
+            hypot = Utils.hypotenuseLength(deltaX, y2)
+            ratio = abs(deltaX / hypot)
+            deltaX *= ratio
 
-            if deltaX != 0.0:
-                y2 = (a * deltaX * deltaX * deltaX) + (b * deltaX * deltaX)
-                hypot = Geometry.hypotenuse(deltaX, y2)
-                ratio = math.abs(deltaX / hypot)
-                deltaX *= ratio
+        self.kRadiusPath = abs(deltaX) * self.UPDATE_RATE * 0.1
+        dx2 = (3.0 * a * deltaX * deltaX) + (2.0 * b * deltaX)
+        relativeFeedForwardAngle = math.atan(dx2)
 
-            kRadiusPath = math.abs(deltaX) * UPDATE_RATE * 0.1
-            dx2 = (3.0 * a * deltaX * deltaX) + (2.0 * b * deltaX)
-            relativeFeedForwardAngle = math.atan(dx2)
+        if False:
+            print(f"{relativeAdjacDist} {relativeOpposDist} {relativeGoalDeriv}")
+            print(f"{a} {b} {deltaX}")
 
-            if False:
-                print(f"{relativeAdjacDist} {relativeOpposDist} {relativeGoalDeriv}")
-                print(f"{a} {b} {deltaX}")
+        turnOutput = -math.degrees(relativeFeedForwardAngle)*self.UPDATE_RATE * self.kTurn
+        outputLeft = ((ffSpeed * self.kV) + turnOutput) * 12.0
+        outputRight = ((ffSpeed * self.kV) - turnOutput) * 12.0
 
-            turnOutput = -math.toDegrees(relativeFeedForwardAngle) * kTurn
-            outputLeft = ((feedForwardSpeed * kV) + turnOutput) * 12.0
-            outputRight = ((feedForwardSpeed * kV) - turnOutput) * 12.0
+        return outputLeft, outputRight
 
-            return outputLeft, outputRight
+    def generateSpline(self, x, y, dx):
+        a = ((x * dx) - (2 * y)) / (x * x * x)
+        b = ((3 * y) - (dx * x)) / (x * x)
+        return a, b
+
+
+
 
 # /**
 #  * Add your docs here.
